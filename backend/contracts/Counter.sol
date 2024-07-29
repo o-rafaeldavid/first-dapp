cdb763fb2d743803196f162b265df96072ed86ab
Rafael David <rafaelcapitaodavid@gmail.com>
2024-07-29 22:23:40 +0100

    prettier

--- backend/contracts/Counter.sol
+++ backend/contracts/Counter.sol
@@ -2,42 +2,30 @@
 pragma solidity ^0.8.19;
 
 contract Counter {
-    string public name;
-    uint256 public count;
+   string public name;
+   uint256 public count;
 
-    event Increment(
-        uint256 when,
-        address indexed who,
-        uint256 byHowMuch,
-        uint256 newCount
-    );
+   event Increment(uint256 when, address indexed who, uint256 byHowMuch, uint256 newCount);
 
-    event Decrement(
-        uint256 when,
-        address indexed who,
-        uint256 byHowMuch,
-        uint256 newCount
-    );
+   event Decrement(uint256 when, address indexed who, uint256 byHowMuch, uint256 newCount);
 
-    constructor(string memory _name, uint256 _initialCount) {
-        name = _name;
-        count = _initialCount;
-    }
+   constructor(string memory _name, uint256 _initialCount) {
+      name = _name;
+      count = _initialCount;
+   }
 
-    function increment() public {
-        count++;
-        emit Increment(block.timestamp, msg.sender, 1, count);
-    }
+   function increment() public {
+      count++;
+      emit Increment(block.timestamp, msg.sender, 1, count);
+   }
 
-    function decrement() public {
-        count--;
-        emit Decrement(block.timestamp, msg.sender, 1, count);
-    }
+   function decrement() public {
+      count--;
+      emit Decrement(block.timestamp, msg.sender, 1, count);
+   }
 
-    function setName(
-        string memory _newName
-    ) public returns (string memory newName) {
-        name = _newName;
-        return name;
-    }
+   function setName(string memory _newName) public returns (string memory newName) {
+      name = _newName;
+      return name;
+   }
 }
--- backend/contracts/Lock.sol
+++ backend/contracts/Lock.sol
@@ -5,38 +5,38 @@ pragma solidity ^0.8.19;
 // import "hardhat/console.sol";
 
 contract Lock {
-    uint256 public unlockTime;
-    address payable public owner;
+   uint256 public unlockTime;
+   address payable public owner;
 
-    event Withdrawal(uint256 amount, uint256 when);
+   event Withdrawal(uint256 amount, uint256 when);
 
-    // Definir erros personalizados
-    error UnlockTimeInFuture();
-    error WithdrawNotAllowed();
-    error NotOwner();
+   // Definir erros personalizados
+   error UnlockTimeInFuture();
+   error WithdrawNotAllowed();
+   error NotOwner();
 
-    constructor(uint256 _unlockTime) payable {
-        if (block.timestamp >= _unlockTime) {
-            revert UnlockTimeInFuture(); // Usando erro personalizado
-        }
+   constructor(uint256 _unlockTime) payable {
+      if (block.timestamp >= _unlockTime) {
+         revert UnlockTimeInFuture(); // Usando erro personalizado
+      }
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
+      unlockTime = _unlockTime;
+      owner = payable(msg.sender);
+   }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+   function withdraw() public {
+      // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+      // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
-        if (block.timestamp < unlockTime) {
-            revert WithdrawNotAllowed(); // Usando erro personalizado
-        }
-        if (msg.sender != owner) {
-            revert NotOwner(); // Usando erro personalizado
-        }
+      if (block.timestamp < unlockTime) {
+         revert WithdrawNotAllowed(); // Usando erro personalizado
+      }
+      if (msg.sender != owner) {
+         revert NotOwner(); // Usando erro personalizado
+      }
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+      emit Withdrawal(address(this).balance, block.timestamp);
 
-        owner.transfer(address(this).balance);
-    }
+      owner.transfer(address(this).balance);
+   }
 }
