d02839190347413005c1afe96aae0d53dc5b2bf8
Rafael David <rafaelcapitaodavid@gmail.com>
2024-08-07 15:32:59 +0100

    RealEstate to Old & 8.20

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,16 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.19",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
+    solidity: {
+        version: '0.8.20',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+            },
+        },
     },
-  },
-};
+}
 
-export default config;
+export default config
